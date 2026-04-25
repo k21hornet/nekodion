316e59d1653d16e10470d1af532dd91954c24805
k21hornet <knt0535.tk@gmail.com>
2026-04-25 23:48:31 +0900

    fix: add logout to error page

--- whisker/src/app/(authorized)/error.tsx
+++ whisker/src/app/(authorized)/error.tsx
@@ -1,9 +1,20 @@
 "use client";
 
+import Link from "next/link";
+import { MdOutlineLogout } from "react-icons/md";
+
 export default function Error() {
   return (
     <div className="flex flex-col items-center justify-center">
       <p>ただいまアクセスできません。しばらくお待ちください。</p>
+      <Link
+        href="/auth/logout"
+        className="flex items-center gap-1"
+        prefetch={false}
+      >
+        ログアウト
+        <MdOutlineLogout className="text-lg" />
+      </Link>
     </div>
   );
 }
