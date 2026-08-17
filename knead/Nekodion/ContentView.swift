d747e216400fb48a6feea91772770bf03bc330d3
k21hornet <knt0535.tk@gmail.com>
2026-08-17 20:17:22 +0900

    feat: init ios

---
+++ knead/Nekodion/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  Nekodion
+//  ContentView.swift
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("子猫こねこね")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ knead/Nekodion/NekodionApp.swift
@@ -0,0 +1,15 @@
+//
+//  Nekodion
+//  NekodionApp.swift
+//
+
+import SwiftUI
+
+@main
+struct NekodionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
